b29b86db6786be7ecb6b3c969d7837768c90d093
sebasGZA <sebassga34@gmail.com>
2026-06-29 19:51:24 -0500

    feat: lowercase,tittlecase and uppercase pipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic',
-  imports: [],
+  imports: [LowerCasePipe, TitleCasePipe, UpperCasePipe],
   templateUrl: './basic-page.component.html',
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+  nameLower = signal('sebastian')
+  nameUpper = signal('SEBASTIAN')
+  fullName = signal('seBasTian gOMez')
+}
