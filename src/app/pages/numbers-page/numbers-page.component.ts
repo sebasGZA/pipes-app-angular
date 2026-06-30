b04d9e45dc52c39e57b8aa8f3dd4fb329e138f00
sebasGZA <sebassga34@gmail.com>
2026-06-29 20:00:35 -0500

    feat: number pipes

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [CurrencyPipe, DecimalPipe, PercentPipe],
   templateUrl: './numbers-page.component.html',
 })
-export default class NumbersPageComponent {}
+export default class NumbersPageComponent {
+  totalSells = signal(2_450_350.2054)
+  percent = signal(0.8560)
+}
