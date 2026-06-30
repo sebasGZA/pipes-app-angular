90490b8fd6ae8405251d49fc91e55c85acb03f5d
sebasGZA <sebassga34@gmail.com>
2026-06-29 20:29:45 -0500

    feat: change language

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localEs  from '@angular/common/locales/es'
+import localEn  from '@angular/common/locales/en'
+import localFr  from '@angular/common/locales/fr'
 
 import { routes } from './app.routes';
+
+registerLocaleData(localEs, 'es')
+registerLocaleData(localEn, 'en')
+registerLocaleData(localFr, 'fr')
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'en'
+    }
   ]
 };
