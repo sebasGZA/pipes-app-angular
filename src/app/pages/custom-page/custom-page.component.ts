5a4296befcaa653e99e679b4e5fc370293078f56
sebasGZA <sebassga34@gmail.com>
2026-06-29 23:52:10 -0500

    feat: HeroText and HeroCreator pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipePipe } from '../../pipes/canfly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipePipe, HeroColorPipe],
+  imports: [
+    ToggleCasePipe, 
+    CanFlyPipePipe, 
+    HeroColorPipe, 
+    HeroCreatorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
